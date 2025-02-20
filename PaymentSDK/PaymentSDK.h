d38a11388d1127aefd74b87c36df7a2a7cd11b72
Maksims Moisja <ma>
2025-02-20 12:06:00 +0200

    Initial Commit

---
+++ PaymentSDK/PaymentSDK.h
@@ -0,0 +1,18 @@
+//
+//  PaymentSDK.h
+//  PaymentSDK
+//
+//  Created by Maksims Moisja on 20/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaymentSDK.
+FOUNDATION_EXPORT double PaymentSDKVersionNumber;
+
+//! Project version string for PaymentSDK.
+FOUNDATION_EXPORT const unsigned char PaymentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymentSDK/PublicHeader.h>
+
+
---
+++ PaymentSDKTests/PaymentSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentSDKTests.swift
+//  PaymentSDKTests
+//
+//  Created by Maksims Moisja on 20/02/2025.
+//
+
+import Testing
+@testable import PaymentSDK
+
+struct PaymentSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
